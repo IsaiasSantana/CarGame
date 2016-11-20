2be1c2b1abef59fc15ec0cbb62b0257d4abb04b5
IsaiasSantana <isds.santana@gmail.com>
2016-11-20 19:05:41 -0300

    remove coment my old project

--- core/src/com/cargame/helpers/AssetLoader.java
+++ core/src/com/cargame/helpers/AssetLoader.java
@@ -14,7 +14,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.net.HttpStatus;
-//import com.dragonGames.gameObjects.Car;
+
 
 public class AssetLoader
 {
